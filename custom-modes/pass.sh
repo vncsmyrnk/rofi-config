d115ac740845b306033053e0e8fefc7d393dbbd2
Vinicius Mayrink <vmayrink12@gmail.com>
2026-02-17 10:35:52 -0300

    feat: adds custom modes
    They can be called as:
    
    ```
    rofi -show pass -modi totp:~/.config/rofi/custom-modes/pass.sh
    ```

---
+++ custom-modes/pass.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pass_names_files=$(fd . --base-directory ~/.password-store --relative-path -t f)
+
+if [[ -n "$*" ]]; then
+  pass -c "$@" >/dev/null || notify-send "Failed to copy password"
+  exit 0
+fi
+
+for f in $pass_names_files; do
+  pass_name=$(echo "$f" | rev | cut -f2- -d "." | rev)
+  echo "$pass_name"
+done
---
+++ custom-modes/totp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ -n "$*" ]]; then
+  util totp generate "$@" >/dev/null || notify-send "Failed to generate TOTP"
+  exit 0
+fi
+
+for file in "$HOME"/.secrets/totp/*; do
+  file_basename_noext=$(basename "$file" | rev | cut -f2- -d "." | rev)
+  echo "$file_basename_noext"
+done
