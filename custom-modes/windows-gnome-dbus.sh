71f966dcbebccd5e0865519ce064a5fecb28cf50
Vinicius Mayrink <vmayrink12@gmail.com>
2026-02-22 20:30:22 -0300

    perf(custom-modes/wind): caches installed apps list

--- custom-modes/windows-gnome-dbus.sh
+++ custom-modes/windows-gnome-dbus.sh
@@ -3,36 +3,53 @@
 # This leverages the GNOME Window Calls extension to "raise or run" applications listed on the rofi's menu
 # Extension: https://github.com/ickyicky/window-calls
 
-if [[ -n "$*" ]]; then
-  # Looks for any opened file
-  gnome_dbus_windows=$(
-    busctl --user call \
-      org.gnome.Shell \
-      /org/gnome/Shell/Extensions/Windows \
-      org.gnome.Shell.Extensions.Windows \
-      List \
-      --json=short
-  )
+CACHE_FILE=/tmp/rofi-applications-list-cache
 
-  mapfile -t desktop_files < <(grep -h "^Name=$*" /usr/share/applications/*.desktop -l)
-  for file in "${desktop_files[@]}"; do
+bustctl_dbus_list_windows() {
+  busctl --user call \
+    org.gnome.Shell \
+    /org/gnome/Shell/Extensions/Windows \
+    org.gnome.Shell.Extensions.Windows \
+    List \
+    --json=short
+}
+
+bustctl_dbus_activate_window() {
+  busctl --user call \
+    org.gnome.Shell \
+    /org/gnome/Shell/Extensions/Windows \
+    org.gnome.Shell.Extensions.Windows \
+    Activate \
+    u "$1"
+}
+
+if [[ -n "$*" ]]; then
+  mapfile -t desktop_filenames_matching_input < <(grep -h "^Name=$*" /usr/share/applications/*.desktop -l)
+  for file in "${desktop_filenames_matching_input[@]}"; do
     wm_class=$(basename "$file" | rev | cut -f2- -d "." | rev)
-    opened_window_ids=$(echo "$gnome_dbus_windows" | jq -r '.data[0]' | jq -r --arg wmClass "$wm_class" '.[] | select(.wm_class == $wmClass) | .id')
+    opened_window_ids=$(
+      bustctl_dbus_list_windows |
+        jq -r '.data[0]' |
+        jq -r --arg wmClass "$wm_class" '.[] | select(.wm_class == $wmClass) | .id'
+    )
+
     if [[ -n "$opened_window_ids" ]]; then
       opened_window_id=$(echo "$opened_window_ids" | tail -n 1)
-      busctl --user call \
-        org.gnome.Shell \
-        /org/gnome/Shell/Extensions/Windows \
-        org.gnome.Shell.Extensions.Windows \
-        Activate \
-        u "$opened_window_id"
+      bustctl_dbus_activate_window "$opened_window_id"
       exit 0
     fi
   done
 
-  # Opens a new instance if not already opened
-  gio launch "${desktop_files[0]}" >/dev/null &
+  gio launch "${desktop_filenames_matching_input[0]}" >/dev/null &
   exit 0
 fi
 
-grep -h "^Name=" /usr/share/applications/*.desktop | cut -d= -f2- | sort -u
+if [[ ! -f "$CACHE_FILE" ]]; then
+  grep -h "^Name=" /usr/share/applications/*.desktop |
+    cut -d= -f2- |
+    sort -u |
+    tee "$CACHE_FILE"
+  exit 0
+fi
+
+cat "$CACHE_FILE"
