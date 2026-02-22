2898cdc7856a5410c4b001f4906b2c569d9a3bab
Vinicius Mayrink <vmayrink12@gmail.com>
2026-02-22 11:58:31 -0300

    fix(custom-modes/opwn): lists the opened windows in reverse order to show the latest focused ones first
    This also cleans up the script breaking `bustctl` calls into separate
    functions.

--- custom-modes/windows-opened-gnome-dbus.sh
+++ custom-modes/windows-opened-gnome-dbus.sh
@@ -3,29 +3,36 @@
 # This leverages the GNOME Window Calls extension to raise opened applications listed on the rofi's menu
 # Extension: https://github.com/ickyicky/window-calls
 
-if [[ -n "$*" ]]; then
-  window_title=$(echo "$*" | cut -d \| -f2 | cut -c2-)
-  window_id=$(busctl --user call \
+bustctl_dbus_list_windows() {
+  busctl --user call \
     org.gnome.Shell \
     /org/gnome/Shell/Extensions/Windows \
     org.gnome.Shell.Extensions.Windows \
     List \
-    --json=short |
-    jq -r '.data[0]' | jq -r --arg windowTitle "$window_title" 'last(.[] | select(.title == $windowTitle) | .id)')
+    --json=short
+}
 
+bustctl_dbus_activate_window() {
   busctl --user call \
     org.gnome.Shell \
     /org/gnome/Shell/Extensions/Windows \
     org.gnome.Shell.Extensions.Windows \
     Activate \
-    u "$window_id"
+    u "$1"
+}
+
+input="$*"
+if [[ -n "$input" ]]; then
+  window_title="${input##*| }"
+  window_id=$(bustctl_dbus_list_windows |
+    jq -r '.data[0]' |
+    jq -r --arg windowTitle "$window_title" 'last(.[] | select(.title == $windowTitle) | .id)')
+
+  bustctl_dbus_activate_window "$window_id"
   exit 0
 fi
 
-busctl --user call \
-  org.gnome.Shell \
-  /org/gnome/Shell/Extensions/Windows \
-  org.gnome.Shell.Extensions.Windows \
-  List \
-  --json=short |
-  jq -r '.data[0]' | jq -r '.[] | (.wm_class // "unknown") + " | " + .title'
+bustctl_dbus_list_windows |
+  jq -r '.data[0]' |
+  jq -r '.[] | (.wm_class // "unknown") + " | " + .title' |
+  tac
