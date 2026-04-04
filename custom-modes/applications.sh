80a4a417eacf49157ba7d3583dbf6f984c8006b0
Vinicius Mayrink <vmayrink12@gmail.com>
2026-04-04 17:53:44 -0300

    chore(applications): caches application listing
    It is sufficiently fast but we don't need to fetch them every time as it
    rarely changes.

--- custom-modes/applications.sh
+++ custom-modes/applications.sh
@@ -3,9 +3,17 @@
 # This leverages the GNOME Window Calls extension to "raise or run" applications listed on the rofi's menu
 # More at: https://github.com/vncsmyrnk/gwin
 
+CACHE_FILE=/tmp/rofi-applications-cache
+
 if [[ -n "$*" ]]; then
   gwin raise "$@" >/dev/null &
   exit 0
 fi
 
-gwin list applications --rofi
+if [[ ! -f "$CACHE_FILE" ]]; then
+  gwin list applications --rofi |
+    tee "$CACHE_FILE"
+  exit 0
+fi
+
+cat "$CACHE_FILE"
