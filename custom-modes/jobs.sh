966466817e43e5aa647f97cc68c2ac9dc1b691d3
Vinicius Mayrink <vmayrink12@gmail.com>
2026-04-11 18:54:46 -0300

    feat(custom-modes): adds `jobs` mode leveraging `util jobs` features

---
+++ custom-modes/jobs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This expectes a profile, lists commands and executes or switch-client if
+# already running
+#
+# More at https://github.com/vncsmyrnk/shell-utils
+
+PROCS_FILE=${USEFUL_URLS_FILE:-~/Documents/Procfile}
+
+mapfile -t windows < <(util jobs list || true)
+
+input="$*"
+if [[ -n "$input" ]]; then
+  case "$input" in
+  "Kill all")
+    util jobs kill --all >/dev/null 2>&1
+    ;;
+  *)
+    if [[ " ${windows[*]} " =~ $input ]]; then
+      util jobs attach "$input"
+      exit 0
+    fi
+    util jobs run "$input" "$ROFI_INFO"
+    ;;
+  esac
+  exit 0
+fi
+
+while IFS= read -r line; do
+  name=$(cut -d':' -f1 <<<"$line")
+  cmd=$(cut -d':' -f2- <<<"$line")
+  if [[ " ${windows[*]} " =~ $name ]]; then
+    active_append=" (running)"
+  fi
+  echo -e "$name\x00info\x1f$cmd\x1fdisplay\x1f$name$active_append"
+done <"$PROCS_FILE"
+
+echo "Kill all"
